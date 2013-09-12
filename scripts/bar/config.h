c740ce83c3550148f7cf2cecf0370f7d8a7b2e72
wei001 <wei001@bucknell.edu>
2013-09-11 22:07:36 -0400

    Added updated panel configs for gradunza5

---
+++ scripts/bar/config.h
@@ -0,0 +1,32 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  14
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+/*#define BAR_FONT 	"-*-tewi-medium-*-*-*-*-*-*-*-*-*-*-*", "fixed"*/
+#define BAR_FONT 	"-*-nu-*-*-*-*-*-*-*-*-*-*-*-*", "fixed"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Define the opacity of the bar (requires a compositor such as compton) */
+#define BAR_OPACITY 1.0 /* 0 is invisible, 1 is opaque */
+/* Color palette */
+#define BACKGROUND  0x181512 /* background */
+#define FOREGROUND  0xd6c3b6 /* foreground */
+#define COLOR0  0x181512 /* background */
+#define COLOR1  0xd6c3b6 /* foreground */
+#define COLOR2  0x332d29
+#define COLOR3  0x8c644c
+#define COLOR4  0x746c48
+#define COLOR5  0xbfba92
+#define COLOR6  0x646a6d
+#define COLOR7  0x766782
+#define COLOR8  0x4b5c5e
+#define COLOR9  0x9a875f
