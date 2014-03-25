b8ae42602fd9025f54934140ce74eb91913a62d3
wei001 <wei001@bucknell.edu>
2014-03-25 12:00:16 -0400

    Updated bspwm config and added bar config

---
+++ .config/bspwm/panel/bar.config.h
@@ -0,0 +1,40 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  14
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+//#define BAR_FONT 	"-lucy-tewi-medium-r-normal--11-90-75-75-p-58-iso10646-1", "fixed"
+//#define BAR_FONT 	"-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1", "-*-lemon-medium-r-normal-*-*-87-*-*-*-*-*-*"
+//#define BAR_FONT 	"", "fixed"
+//#define BAR_FONT 	"-*-tamsyn-medium-r-normal-*-*-87-*-*-*-*-*-*","-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1"
+//#define BAR_FONT 	"-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1","-lucy-tewi-medium-r-normal--11-90-75-75-p-58-iso10646-1"
+//#define BAR_FONT 	"-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1","-*-lemon-medium-r-normal-*-*-*-*-*-*-*-*-*"
+#define BAR_FONT 	"-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1","-lokaltog-fixed-medium-r-normal--10-100-75-75-c-60-iso8859-1"
+//#define BAR_FONT 	"-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1","-lokaltog-fixed-medium-r-normal--10-100-75-75-c-60-iso8859-1"
+//#define BAR_FONT 	"-misc-stlarch-medium-r-normal--10-100-75-75-c-80-iso10646-1","-lokaltog-fixed-medium-*-normal--*-*-*-*-*-*-*-*"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Define the opacity of the bar (requires a compositor such as compton) */
+#define BAR_OPACITY 1.0 /* 0 is invisible, 1 is opaque */
+/* Color palette */
+#define BACKGROUND  0x181512 /* background */
+#define FOREGROUND  0xbea492 /* foreground */
+#define COLOR0  0x181512 /* background */
+#define COLOR1  0xbea492 /* foreground */
+#define COLOR2  0x332d29
+#define COLOR3  0x8c644c
+#define COLOR4  0x746c48
+#define COLOR5  0xbfba92
+#define COLOR6  0x646a6d
+#define COLOR7  0x817267
+#define COLOR8  0x504339
+#define COLOR9  0x817267
+
